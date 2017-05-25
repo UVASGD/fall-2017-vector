794832b8f9643b4378e261adff69d1dcfa2a3db4
WyotJunctor <wjoyner127@gmail.com>
2017-05-25 08:13:50 -0400

    player can move+dash using simple system which we will want to replace, also minor bug fixes

--- GameTest/Assets/Scripts/Person.cs
+++ GameTest/Assets/Scripts/Person.cs
@@ -20,19 +20,19 @@ public class Person {
     int size;
 
     public Person(string b, string n, string t, float l, int s, string sc) {
-        renderName = b;
-        objectName = n;
-        type = t;
-        loc = l;
-        size = s;
-        sortingLayerNum = size.ToString();
-        script = sc;
+        renderName = b; //Name of the sprite rendering prefab found in Resources
+        objectName = n; //Name of the actual person game object once in play
+        type = t; //Name of the prefab of the actual person object found in Resources
+        loc = l; //Location of the object
+        size = s; //Size of the person. For now, please keep sizes from 0 through 6
+        sortingLayerNum = size.ToString(); //Where the gameobject is placed in the sorting layer. This will only affect render order, so objects can still collide
+        script = sc; //The name of the script that should be attached to the object
 
-        setObject();
+        SetObject(); //Establishes the actual game object with size and location and collider
 
-        setHealthBar();
+        SetHealthBar(); //Sets the healthbar. Make sure this comes before setBody
 
-        setBody();
+        SetBody(); //Sets the body render
     }
 
 
@@ -46,23 +46,24 @@ public class Person {
 		
 	}
 
-    public void setObject() {
+    public void SetObject() {
         person = (GameObject)MonoBehaviour.Instantiate(Resources.Load(type), new Vector3(loc, 0, 0), Quaternion.identity);
         person.AddComponent(System.Type.GetType("Personality"));
         BoxCollider2D coll = person.GetComponent<BoxCollider2D>();
+        coll.isTrigger = true;
         coll.size = new Vector3(size*2, 1f, 0f);
         person.AddComponent(System.Type.GetType(script));
         person.name = objectName;
     }
 
-    public void setBody() {
+    public void SetBody() {
         body = (GameObject)MonoBehaviour.Instantiate(Resources.Load(renderName), person.transform);
         SpriteRenderer bodyRender = body.GetComponent<SpriteRenderer>();
 
         bodyRender.sortingLayerName = sortingLayerNum;
     }
 
-    public void setHealthBar() {
+    public void SetHealthBar() {
         healthBar = (GameObject)MonoBehaviour.Instantiate(Resources.Load("RHealthBar"), person.transform);
         SpriteRenderer healthRender = healthBar.GetComponent<SpriteRenderer>();
 
--- GameTest/Assets/Scripts/PlayerScript.cs
+++ GameTest/Assets/Scripts/PlayerScript.cs
@@ -7,7 +7,13 @@ public class PlayerScript : MonoBehaviour {
     public TimeManager time;
     Personality mind;
 
-    int clocks = 0;
+    int obsTimer = 4;
+    int moveTimer = 2;
+    int actTimer = 2;
+
+    char moveQ = 'w';
+    float dashTime = 0.5f;
+
 
 
     public PlayerScript() {
@@ -22,16 +28,83 @@ public class PlayerScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+        //CONTROLS HERE
+        if (Input.anyKeyDown) {
+            int moveKey = (int)Input.GetAxisRaw("Horizontal");
+            if (moveQ == 'w') {
+                dashTime = 0.5f;
+                if (moveKey == 1) {
+                    moveQ = 'l';
+                    moveTimer = 2;
+                }
+                else if (moveKey == -1) {
+                    moveQ = 'r';
+                    moveTimer = 2;
+                }
+            }
+            else if (dashTime > 0){
+                if (moveQ == 'l' && moveKey == 1) {
+                    moveQ = 'L';
+                    moveTimer = 1;
+                    dashTime = 0;
+                }
+                else if (moveQ == 'r' && moveKey == -1) {
+                    moveQ = 'R';
+                    moveTimer = 1;
+                    dashTime = 0;
+                }
+            }
+        }
+
+        if (dashTime > 0)
+            dashTime -= Time.deltaTime;
+
         if (time.clock)
-            Act();
+            Perform();
 	}
 
-    void Act() {
-        if (clocks % 2 == 0)
-            Debug.Log("TICK");
-        else
-            Debug.Log("TOCK");
-        clocks++;
-        //mind.Cool();
+    void Perform() {
+        if (moveTimer == 0) {
+            moveTimer = Move();
+            //mind.Cool();
+        }
+
+        moveTimer--;
+    }
+
+    int Observe() {
+        return 4;
+    }
+
+    int Move() {
+        switch (moveQ) {
+            case 'l':
+                gameObject.transform.Translate(1, 0, 0);
+                moveQ = 'w';
+                break;
+            case 'L':
+                gameObject.transform.Translate(1, 0, 0);
+                moveQ = 'l';
+                moveTimer = 1;
+                break;
+            case 'r':
+                gameObject.transform.Translate(-1, 0, 0);
+                moveQ = 'w';
+                break;
+            case 'R':
+                gameObject.transform.Translate(-1, 0, 0);
+                moveQ = 'r';
+                moveTimer = 1;
+                break;
+            case 'w':
+                moveTimer++;
+                break;
+        }
+
+        return moveTimer;
+    }
+
+    int Act() {
+        return 2;
     }
 }
