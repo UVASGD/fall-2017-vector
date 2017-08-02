5711b604b6ac954c13e4f0396674684ad6a11b7b
WyotJunctor <wjoyner127@gmail.com>
2017-08-02 11:39:20 -0400

    Identity + Mark

---
+++ GameTest/Assets/Scripts/MentalScripts/Identity.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Couriers
+ * Economizers
+ * East Gov
+ * West Gov
+ * Sanctum Arcanum
+ * Those Knights, tho
+ * Middleburg
+ * Middler Monks
+ * Mild Bandits
+ * Wild Bandits
+ * Cultists
+ * 
+ * 
+ * */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Identity {
+
+    List<Mark> marks;
+    public Mark[] Marks { get { return marks.ToArray(); } }
+
+    
+
+    public void AddMark(Mark _mark) {
+        marks.Add(_mark);
+    }
+
+    public bool RemoveMark(Mark _mark) {
+        return marks.Remove(_mark);     
+    }
+
+}
+
+public class PersonIdentity : Identity { }
+
+public class FactionIdentity : Identity { }
+
+public class PlaceIdentiy : Identity { }
+
+public class ItemIdentity : Identity { }
+
