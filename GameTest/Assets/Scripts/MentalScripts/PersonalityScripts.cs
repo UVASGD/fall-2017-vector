a1df30a7b3af9a37bc8ffe4140d730b548ffb0d2
WyotJunctor <wjoyner127@gmail.com>
2017-09-18 22:25:12 -0400

    Updated all dictionaries in Personality Script to utilize structs. Perceive function is finished. Various other minuscule fixes. Feel function to be completed next!

--- GameTest/Assets/Scripts/MentalScripts/PersonalityScripts.cs
+++ GameTest/Assets/Scripts/MentalScripts/PersonalityScripts.cs
@@ -7,6 +7,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Interaction {
+    float capacity;
+    public float Capacity { get { return capacity; } set { capacity = value; } }
+
+    float polarity;
+    public float Polarity { get { return polarity; } }
+
+    float strength;
+    public float Strength { get { return strength; } }
+
+    public Interaction(float _polarity, float _strength, int _capacity = 0) {
+        polarity = _polarity;
+        strength = _strength;
+        capacity = _capacity;
+    }
+
+    public void Apply(float polarityDelt = 0, float strengthDelt = 0, float capacityDelt = 0) {
+        polarity += polarityDelt;
+        strength += strengthDelt;
+        capacity += capacityDelt;
+    }
+}
+
 public class Association {
     string name; //The name that will appear in 'dialogue' strings
     public string Name { get { return name; } }
@@ -17,13 +40,18 @@ public class Association {
     float interest;
     public float Interest { get { return interest; } }
 
-    public Dictionary<string, float[]> associations; //The assocs this is connected to [0] being polarity, [1] being strength, [2] being exhaustion multiplier
-    public Dictionary<string, float[]> marks; //The associations to which this association is 'perma' connected to
+    float accesses;
+    public float Accesses { set { accesses = value; } get { return accesses; } } 
 
-    public Association(string _name, string _id, Dictionary<string, float[]> _marks = null) {
+    public Dictionary<string, Interaction> associations; //The assocs this is connected to [0] being polarity, [1] being strength
+    public Dictionary<string, Interaction> addToMarks; //This is a temporary list of strings to association values
+    public Dictionary<Association, Interaction> marks; //The associations to which this association is 'perma' connected to, [2] being exhaustion multiplier
+
+    public Association(string _name, string _id, Dictionary<string, Interaction> _marks = null) {
         name = _name;
         id = _id;
-        marks = _marks;
+        addToMarks = _marks;
+        accesses = 0;
     }
 
 
@@ -31,10 +59,6 @@ public class Association {
     }
 
     public void AddAssoc(string n, float[] interaction) {
-        if (associations.ContainsKey(n)) {
-            for (int i = 0; i < associations[n].Length - 1; i++)
-                break;
-        }
     }
 }
 
@@ -45,7 +69,7 @@ public class VerbAssoc : Association {
     float polarity; //Kindness of the interaction
     public float Polarity { get { return polarity; } }
 
-    public VerbAssoc(string _name, string _id, string _gerund, float _polarity, Dictionary<string, float[]> _marks = null) : 
+    public VerbAssoc(string _name, string _id, string _gerund, float _polarity, Dictionary<string, Interaction> _marks = null) : 
                      base(_name, _id, _marks) {
         gerund = _gerund;
         polarity = _polarity;
@@ -57,32 +81,60 @@ public class VerbAssoc : Association {
 }
 
 public class MoodAssoc : Association {
-    public MoodAssoc(string _name, string _id, Dictionary<string, float[]> _marks = null) : 
+    public MoodAssoc(string _name, string _id, Dictionary<string, Interaction> _marks = null) : 
                      base(_name, _id, _marks) {
     }
 }
 
 public class PersonAssoc : Association {
-    public PersonAssoc(string _name, string _id, Dictionary<string, float[]> _marks = null) :
+    public PersonAssoc(string _name, string _id, Dictionary<string, Interaction> _marks = null) :
                        base(_name, _id, _marks) {
     }
 }
 
 public class PlaceAssoc : Association {
-    public PlaceAssoc(string _name, string _id, Dictionary<string, float[]> _marks = null) :
+    public PlaceAssoc(string _name, string _id, Dictionary<string, Interaction> _marks = null) :
                       base(_name, _id, _marks) {
     }
 }
 
 public class ItemAssoc : Association {
-    public ItemAssoc(string _name, string _id, Dictionary<string, float[]> _marks = null) :
+    public ItemAssoc(string _name, string _id, Dictionary<string, Interaction> _marks = null) :
                      base(_name, _id, _marks) {
+    }
+}
+
+public struct EventInfo {
+    float interest;
+    public float Interest { get { return interest; } }
+
+    float polarity;
+    public float Polarity { get { return polarity; } }
+
+    float strength;
+    public float Strength { get { return strength; } }
+
+    int accesses;
+    public int Accesses { get { return accesses; } }
+
+    public EventInfo(float _interest, float _polarity, float _strength, int _accesses = 1) {
+        polarity = _polarity;
+        strength = _strength;
+        interest = _interest;
+        accesses = _accesses;
+    }
+
+    public void Apply(float interestDelt = 0, float polarityDelt = 0, float strengthDelt = 0, int accessesDelt = 0) {
+        interest += interestDelt;
+        polarity += polarityDelt;
+        strength += strengthDelt;
+        accesses += accessesDelt;
     }
 }
 
 public class Personality {
     List<Association> associator;
-    Dictionary<string[], float[]> seenEvents; //interest[0], interaction polarity [1], interaction [2], # times [3]
+    Dictionary<string[], EventInfo> seenEvents; //interest[0], interaction polarity [1], interaction [2], # times [3]
     Identity identity;
 
     float totalInterest;
@@ -93,7 +145,15 @@ public class Personality {
 
     float div = 1;
 
-    public void Perceive(string[] info, float[] interaction) {
+    public Personality() {
+        foreach (Association a in associator)
+            foreach (string s in a.addToMarks.Keys)
+                foreach (Association aOther in associator)
+                    if (aOther.Id.Equals(s))
+                        a.marks.Add(aOther, a.addToMarks[s]);
+    }
+
+    public void Perceive(string[] info, Interaction interaction) {
 
         Association subj = null;
         VerbAssoc vb = null;
@@ -111,182 +171,24 @@ public class Personality {
             }
         }
 
+        //Logic in here to detect whether subj, vb, or obj are still null!
+
         if (seenEvents.ContainsKey(info)) {
             div = 0.25f;
-            //process all emotions at 1 / 4
-            float tempInt = seenEvents[info][0];
-            seenEvents[info][0] = tempInt + vb.Interest;
+            seenEvents[info].Apply(interestDelt: vb.Interest);
         }
 
-        else { }
+        else {
+            totalInterest += subj.Interest + vb.Interest + ((obj != null) ? obj.Interest : 0);
+            seenEvents.Add(info, new EventInfo(totalInterest, interaction.Polarity, interaction.Strength));
+            div = (totalInterest > interestThreshold) ? 1 : 0.25f;
+        }
 
-        //Check whether this event has already been witnessed
-        //If it has, process all emotions at 1 / 4 !
-        //Only add the interest of the verb to the acculumative interest
-
-        //If it hasn't been witnessed, check whether it's passed the interest threshold
-
-        //If it has
-        //Process all emotions at full capacity
-        //Add to witnessed events
-        
-        //Check whether the seen event passes any interest threshold
-
+        Feel(subj, obj, vb, div);
     }
 
-    public void Feel(string info, int div, bool opine = false) { }
+    public void Feel(Association _subj, Association _obj, Association _vb, float div, bool opine = false) { }
 
-    public void Feel(string identity) { }
+    public void Feel(Association _concept) { }
 
 }
-/*
-// enum Moods { Charm, Disgust, Amuse, Anger, Inspire, Intimidate, Happy, Sad, Hinder, Harm, Heal, Expedite };
-
-    public enum Mood {
-        Charm, Amuse, Inspire, Happy,
-        Disgust, Anger, Intimidate, Sad,
-        None
-    };
-
-    float[] quants = { 0, 0, 0, 0 };
-
-    float[] thresholds = {  1,  1,  1,  1,
-                           -1, -1, -1, -1 };
-
-    float[] mods = { 1, 1, 1, 1,
-                     1, 1, 1, 1 };
-
-    float[] cooldowns = { 0.1f, 0.1f, 0.1f, 0.1f,
-                          0.1f, 0.1f, 0.1f, 0.1f };
-
-    bool[] redMods = { false, false, false, false,
-                       false, false, false, false};
-
-    bool[] newTraits = { false, false, false, false,
-                         false, false, false, false};
-
-    Mood dominant;  // This is the dominating mood of the personality, which will be visible to the player
-
-    List<PTrait>[] traits = new List<PTrait>[8];  // An array of eight generic lists of Personality Traits, one list for each mood
-
-    public float GetMod(Mood m) {  // Returns the mod of the given mood
-        return mods[(int)m];
-    }
-
-    public void ChangeMod(Mood m, float delt) {  // Changes the modifier for a given mood by the given delta
-        mods[(int)m] += delt;
-    }
-
-    public float GetQuant(Mood m) {
-        return quants[(int)m % 4];
-    }
-
-    public void ChangeQuant(Mood m, float delt) {  // Changes the quantity of the given mood, interpreting the delta as a magnitudue of change for the negative-defined moods.
-        if ((int)m < 4)
-            quants[(int)m] += delt;
-        else
-            quants[(int)m % 4] -= delt;
-        //quants[(int)m % 4] += delt * (mods[(int)m]);  <- OLD CODE; REPLACED BY ABOVE
-    }
-
-    public void ChangeQuant(Mood m, float delt, float sMod) { // Changes the quantity of the given mood, interpreting the delta as a magnitudue of change for the negative-defined moods.
-        if ((int)m < 4)
-            quants[(int)m] += delt * sMod;
-        else
-            quants[(int)m % 4] -= delt * sMod;
-        // quants[(int)m % 4] += delt * sMod;  <- OLD CODE; REPLACED BY ABOVE
-    }
-
-    public void ActivateMood(Mood m, float quant=0f) {  // 
-        int redirectSpot = -1;
-        // If the mood has new traits that haven't been checked for redirects yet, perform check
-        if (newTraits[(int)m])
-            redirectSpot = CheckRedirect(m);
-        // If a redirect was found, perform the redirect
-        if (redirectSpot != -1)
-            traits[(int)m][redirectSpot].Check(m, quant);
-        // If a new redirect was not found, then activate all traits
-        else
-            foreach (PTrait trait in traits[(int)m]) {
-                trait.Check(m, quant);  // TODO: Consider passing in the mood being activated to the trait to ensure traits can act on the right mood
-                ChangeQuant(m, quant, mods[(int)m]);  // Make Change
-            }
-        DetermineDominant();  // Check if dominant mood has changed.
-    }
-
-    public int CheckRedirect(Mood m) {  // Checks whether mood 'm' has a redirect trait
-        int spot = -1;  // Index of redirect trait in traits list
-        newTraits[(int)m] = false;  // Unsets dirty bit.
-        for (int i = 0; i < traits[(int)m].Count; i++) {  // loops through traits list
-            if (!redMods[i])
-                continue;  // If the mood is already known to have a redirect, no check is made
-
-            PTrait trait = traits[(int)m][i];
-            if (trait.GetType().Equals("Redirect")) {
-                redMods[i] = true;  // Flag is raised indicating the mood has a redirect
-                return i;  // A redirect trait has been found, so the search is ended
-                // spot = i;
-            }
-        }
-        return spot;
-    }
-
-    public void Cool() {  // Performs cooldowns on emotions (reducing their magnitude, pos or neg)
-        for (int i = 0; i < 4; i++) {
-            if (quants[i] > 0)  // If the track is a positive emotion, decrease it by the cooldown
-                quants[i] = Mathf.Max(quants[i] - cooldowns[i], 0f);  // Decreases emotion without going below 0
-            else if (quants[i] < 0)  // If the track is a negative emotion, increase it by the cooldown
-                quants[i] = Mathf.Min(quants[i] + cooldowns[i + i], 0f); // Increases emotion without going above 0
-        }
-    }
-
-    public void DetermineDominant() {
-        Mood newDom = Mood.None;  // Default to no mood
-        float highestQuant = 0f;  // track maginitude of highest mood quantity
-        for (int i = 0; i < 8; i++) {
-            float thisQuant = Mathf.Abs(quants[i]);
-            // if the current mood has higher magnitude than the previous highest and is above its thrshold, it is dominant
-            if (thisQuant > highestQuant && thisQuant > thresholds[i]) {
-                newDom = (Mood) i;
-                highestQuant = thisQuant;
-            }
-        }
-        dominant = newDom;
-    }
-
-    public Mood GetDominant() {
-        return dominant;
-    }
-
-    public string domStr() {
-        string returnVal = "None";
-        switch (dominant) {
-            case (Personality.Mood.Charm): returnVal = "Charm"; break;
-            case (Personality.Mood.Amuse): returnVal = "Amuse"; break;
-            case (Personality.Mood.Inspire): returnVal = "Inspire"; break;
-            case (Personality.Mood.Happy): returnVal = "Happy"; break;
-
-            case (Personality.Mood.Disgust): returnVal = "Disgust"; break;
-            case (Personality.Mood.Anger): returnVal = "Anger"; break;
-            case (Personality.Mood.Intimidate): returnVal = "Intimidate"; break;
-            case (Personality.Mood.Sad): returnVal = "Sad"; break;
-        }
-        return returnVal;
-    }
-
-    public void AddTrait(Mood m, PTrait trait) {
-        traits[(int)m].Add(trait);
-        traits[(int)m][traits[(int)m].Count - 1].person = this;
-    }
-
-    public bool IsOverThreshold(Mood m) {
-        int moodInt = (int)m;
-        float quant = quants[moodInt % 4];
-        if (moodInt < 4)
-            return quant >= thresholds[moodInt];
-        else
-            return quant <= thresholds[moodInt];
-    }
-    }
-    */
-
