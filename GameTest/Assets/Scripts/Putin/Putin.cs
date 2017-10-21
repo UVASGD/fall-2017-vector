6e7ccea035f1f554e37d6d62d56a680fb8e8f4e4
SimonMarkWhittle <smw2ed@virginia.edu>
2017-10-20 23:59:48 -0400

    Made Putin a thing

---
+++ GameTest/Assets/Scripts/Putin/Putin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Putin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey || Input.GetMouseButton(0) || Input.GetMouseButton(1)) {
+            SceneManager.LoadScene("Menu");
+        }
+	}
+}
